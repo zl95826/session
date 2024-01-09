a0024948f36af0faf25e0212c081557e26d7de65
zl95826 <zl95826@hotmail.com>
2024-01-08 21:25:23 -0800

    initial commit

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const app = express();
+app.use(express.json());
+app.use((req, res, next) => {
+  console.log("Time: ", Date.now());
+  next();
+});
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+app.get("/error", (req, res, next) => {
+  throw "Error here";
+});
+app.use("/user/:id", (req, res, next) => {
+  console.log("Request Type:", req.method);
+  next();
+});
+app.get("/user/:id", (req, res, next) => {
+  console.log(req.params, req.body);
+  //If you want to access the data sent in the request body (req.body),
+  // you could use the built-in middleware called express.json() in Express.js.
+  //For req.params, you don't need it
+  res.send("User " + req.params.id);
+});
+//Middleware can also be declared in an array for reusability.
+const logOriginalUrl = (req, res, next) => {
+  console.log("Request URL:", req.originalUrl);
+  next();
+};
+const logMethod = (req, res, next) => {
+  console.log("Request Type:", req.method);
+  next();
+};
+const logStuff = [logOriginalUrl, logMethod];
+app.get("/users", logStuff, (req, res, next) => {
+  res.send("Users Info");
+});
+app.use((req, res, next) => {
+  res.status(404).send("404 errors");
+});
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.status(500).send("error happens");
+});
+app.listen(3000, () => {
+  console.log("Server is working");
+});
